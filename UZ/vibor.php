7ef20be68cc5a74dad6e8c1e9393c5827869f108
Eshchanov <rasool@inbox.ru>
2017-11-15 14:27:36 +0500

    sdd

--- UZ/vibor.php
+++ UZ/vibor.php
@@ -7,7 +7,7 @@
 
 <body>
 <div class="vibor container well well-lg">
-<h5>O`zbekiston Respublikasi dsadsadfsafsafuqarolari uchun interaktiv xizmatlar: </h5>
+<h5>O`zbekiston Respublikasi fuqarolari uchun interaktiv xizmatlar: </h5>
 <div class="k-uslugi">
 
 <div class="row">
